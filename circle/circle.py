dc009f9b633dab8b11041b3054fbe809654b7c5b
Hanna <hanna.bahaukova@mail.ru>
2021-12-17 21:13:42 +0000

    circle_1

--- circle/circle.py
+++ circle/circle.py
@@ -3,7 +3,7 @@
 # Бабушка Зина любит печь блины своему любимому внуку Васе.
 # А внук Вася любит математику и знает, что периметр и площадь блина можно найти по диаметру сковородки.
 # Напишите программу, которая поможет Васе проверить его вычисления.
-<< << << < HEAD
+
 from math import pi
 
 
@@ -14,19 +14,6 @@ def perimeter(diameter):
 def area(diameter):
     r = diameter / 2
     return pi * r ** 2
-
-== == == =
-
-def perimeter(diameter):
-    pass
-
-
-def area(diameter):
-    pass
-
->> >> >> > 111
-fee6(Initial
-commit)
 
 
 if __name__ == "__main__":
