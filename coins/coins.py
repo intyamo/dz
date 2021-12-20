22e8d4c574f1d76a709df5e857d366736d44d4cf
zharkovdmitrii9911 <56621122+zharkovdmitrii9911@users.noreply.github.com>
2021-12-20 16:53:01 +0300

    Zharkov

--- coins/coins.py
+++ coins/coins.py
@@ -5,12 +5,20 @@
 # Номинальная стоимость монет: a, b и с тугриков.
 
 def max_sum_of_2(a: int, b: int, c: int) -> int:
-    pass
-
+    ab = a+b
+    ac = a+c
+    bc = b+c
+    if ab > ac and ab > bc:
+        return ab
+    else:
+        if bc > ac:
+            return bc
+        else:
+            return ac
 
 if __name__ == "__main__":
     a = int(input("a = "))
-    b = int(input("a = "))
-    c = int(input("a = "))
+    b = int(input("b = "))
+    c = int(input("c = "))
 
     print(max_sum_of_2(a, b, c))
