31cace88494d00412fa43a933a7364e577272d1a
Koval Ihar <Helter198931@mail.ru>
2021-12-24 15:08:34 +0300

    verification tasks

--- coins/coins.py
+++ coins/coins.py
@@ -3,14 +3,23 @@
 # Бабушка Зина даёт своему любимому внуку Васе 3 монеты и разрешает оставить 2 из них.
 # Найдите максимальную сумму из любых двух монет.
 # Номинальная стоимость монет: a, b и с тугриков.
+from unittest import result
+
 
 def max_sum_of_2(a: int, b: int, c: int) -> int:
-    pass
+    sum = max(a+b, b+c, a+c)
+
+
+
+    return sum
 
 
 if __name__ == "__main__":
     a = int(input("a = "))
     b = int(input("a = "))
     c = int(input("a = "))
+
+
+
 
     print(max_sum_of_2(a, b, c))
