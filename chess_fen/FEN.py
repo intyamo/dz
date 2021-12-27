571460e1725a956046597f3a622b6c1a025b0e2d
Ivan_Malyshka <malyshko.ivan1@gmail.com>
2021-12-27 16:06:56 +0300

    dz_2_str

--- chess_fen/FEN.py
+++ chess_fen/FEN.py
@@ -39,10 +39,57 @@ KNIGHT_VAL = BISHOP_VAL = 3  # конь и слон
 ROOK_VAL = 5  # ладья
 QUEEN_VAL = 9  # ферзь
 
-
+# ready
 def calc_chess_balance(fen: str) -> int:
-    pass
+    balance = 0
+    for i in fen:
+        if i == 'Q':
+            balance += QUEEN_VAL
+        elif i == 'R':
+            balance += ROOK_VAL
+        elif i == 'B' or i == 'N':
+            balance += BISHOP_VAL
+        elif i == 'P':
+            balance += PAWN_VAL
+        elif i == 'q':
+            balance -= QUEEN_VAL
+        elif i == 'r':
+            balance -= ROOK_VAL
+        elif i == 'b' or i == 'n':
+            balance -= BISHOP_VAL
+        elif i == 'p':
+            balance -= PAWN_VAL
+    return balance
 
 
 def chess_board(fen: str) -> str:
-    pass
+    index_el = fen.find(' ')
+    field = fen[:(index_el + 1)]
+    chisl_matric = len(field)
+    board = ''
+    aboard = ''
+    for f in range(0, chisl_matric):
+        element_fen = field[f]
+        if len(aboard) == 16:
+            board += aboard.lstrip() + '\n'
+            aboard = ''
+        else:
+            if element_fen.isdecimal() == True:
+                aboard += ' .' * int(element_fen)
+            elif element_fen == '/':
+                aboard += ''
+            else:
+                aboard += ' ' + element_fen
+    chess = board.splitlines()
+    chess_board = f'''\
+    {chess[0]}
+    {chess[1]}
+    {chess[2]}
+    {chess[3]}
+    {chess[4]}
+    {chess[5]}
+    {chess[6]}
+    {chess[7]}\
+    '''
+    return chess_board
+print(chess_board("4r2k/q5bp/4R3/4P1P1/P1Qn3R/2N5/1r4KP/8 w - - 1 43"))
