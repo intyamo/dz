04138516e4499d708d9bbb5f67bfa00d248fbec4
Andrei_petrusevich <petrusevich83@gmail.com>
2022-02-01 19:16:20 +0300

    Find lessons time

--- rectangles/rectangles.py
+++ rectangles/rectangles.py
@@ -24,11 +24,30 @@ def rect_inside(a: Rect, b: Rect) -> bool:
     """
     Checks if whole rectangle `b` is within rectangle `a`.
     """
-    pass
+    if b[0] >= a[0] and b[1] >= a[1] and b[2] + b[0] <= a[2] + a[0] and b[3] + b[1] <= a[3] + a[1]:
+        return True
+    else:
+        return False
 
 
 def rects_intersect(a: Rect, b: Rect) -> bool:
     """
     Checks if 2 rectangles `a` and `b` have at least a single intersection point.
     """
-    pass
+    if (a[0] <= b[0] <= a[0] + a[2] and a[1] <= b[1] <= a[1] + a[3]
+            or a[0] <= b[0] <= a[0] + a[2] and a[1] <= b[1] + b[3] <= a[1] + a[3]
+            or a[0] <= b[0] + b[2] <= a[0] + a[2] and a[1] <= b[1] + b[3] <= a[1] + a[3]
+            or a[0] <= b[0] + b[2] <= a[0] + a[2] and a[1] <= b[1] <= a[1] + a[3]):
+        return True
+    else:
+        return False
+
+
+a = Rect(0, 1, 15, 20)
+b = Rect(-5, 0, 8, 8)
+print('Прямоугольник а', a)
+print('Прямоугольник b', b)
+print('____')
+print('принадлежат ли все точки второго прямоугольника первому', rect_inside(a, b))
+print('____')
+print('пересекаются ли эти прямоугольники', rects_intersect(a, b))
